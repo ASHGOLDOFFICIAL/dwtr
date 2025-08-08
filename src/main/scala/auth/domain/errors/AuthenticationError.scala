eeac0a72a89fda2150791baf21685dec00954d29
ashgoldofficial <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2025-08-08 13:18:35 +0500

    Removed no longer used auth errors.

--- src/main/scala/auth/domain/errors/AuthenticationError.scala
+++
@@ -1,8 +0,0 @@
-package org.aulune
-package auth.domain.errors
-
-import scala.util.control.NoStackTrace
-
-
-enum AuthenticationError extends NoStackTrace:
-  case InvalidCredentials
--- src/main/scala/auth/domain/errors/LoginError.scala
+++
@@ -1,9 +0,0 @@
-package org.aulune
-package auth.domain.errors
-
-import scala.util.control.NoStackTrace
-
-
-enum LoginError extends NoStackTrace:
-  case UserNotFound
-  case InvalidCredentials
