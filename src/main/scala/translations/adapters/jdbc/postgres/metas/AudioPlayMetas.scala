a87add4bf7126e20b3487785844d69e96e96ce68
ashgoldofficial <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2025-08-13 15:28:27 +0500

    Added Meta for ExternalResourceType

--- src/main/scala/translations/adapters/jdbc/postgres/metas/AudioPlayMetas.scala
+++ src/main/scala/translations/adapters/jdbc/postgres/metas/AudioPlayMetas.scala
@@ -6,6 +6,7 @@ import translations.domain.model.audioplay.{
   AudioPlaySeriesNumber,
   AudioPlayTitle
 }
+import translations.domain.shared.ExternalResourceType
 
 import doobie.Meta
 
@@ -20,3 +21,15 @@ private[postgres] object AudioPlayMetas:
     AudioPlaySeriesNumber(str).toRight(
       s"Failed to decode AudioPlaySeriesNumber from: $str.")
   }(identity)
+
+  given Meta[ExternalResourceType] =
+    val toInt: Map[ExternalResourceType, Int] =
+      ExternalResourceType.values.map {
+        case t @ ExternalResourceType.Purchase  => t -> 1
+        case t @ ExternalResourceType.Streaming => t -> 2
+        case t @ ExternalResourceType.Download  => t -> 3
+        case t @ ExternalResourceType.Other     => t -> 4
+        case t @ ExternalResourceType.Private   => t -> 5
+      }.toMap
+    val fromInt = toInt.map(_.swap)
+    Meta[Int].timap(fromInt.apply)(toInt.apply)
