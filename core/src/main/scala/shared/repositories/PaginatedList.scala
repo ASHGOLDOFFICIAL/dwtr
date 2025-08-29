dbcb2cd5c99a3fad4da3cfbdea52d367df812bb2
ashgoldofficial <104313094+ASHGOLDOFFICIAL@users.noreply.github.com>
2025-08-29 08:04:34 +0500

    Renamed token to cursor.

--- core/src/main/scala/shared/repositories/PaginatedList.scala
+++ core/src/main/scala/shared/repositories/PaginatedList.scala
@@ -3,16 +3,14 @@ package shared.repositories
 
 
 /** List operation for repository with support of cursor-based pagination.
- *
  *  @tparam F effect type.
  *  @tparam E element type.
- *  @tparam Token type of element used as cursor.
+ *  @tparam Cursor type of element to use as a cursor.
  */
-trait PaginatedList[F[_], E, -Token]:
+trait PaginatedList[F[_], E, -Cursor]:
   /** List contained elements.
-   *
-   *  @param startWith optional token with information for continued listing.
+   *  @param startWith optional cursor with information for continued listing.
    *  @param count number of elements to return.
    *  @return list of elements.
    */
-  def list(startWith: Option[Token], count: Int): F[List[E]]
+  def list(startWith: Option[Cursor], count: Int): F[List[E]]
